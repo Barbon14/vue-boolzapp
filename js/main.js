8044a9ac7f1a50b955cc5b4e37fa9520d223f276
BarbonFisso <barbieri.mat@hotmail.it>
2021-11-16 16:26:26 +0100

    added time info to messages

--- js/main.js
+++ js/main.js
@@ -11,17 +11,17 @@ var app = new Vue({
                 visible : true,
                 messages : [
                     {
-                        date : '10/01/2020 1 5:30:55',
+                        date : '10/01/2020 15:30:55',
                         text : 'Hai portato a spasso il cane?',
                         status : 'sent'
                     },
                     {
-                        date : '10/01/2020 1 5:50:00',
+                        date : '10/01/2020 15:50:00',
                         text : 'Ricordati di dargli da mangiare',
                         status : 'sent'
                     },
                     {
-                        date : '10/01/2020 1 6:15:22',
+                        date : '10/01/2020 16:15:22',
                         text : 'Tutto fatto!',
                         status : 'received'
                     }
@@ -33,17 +33,17 @@ var app = new Vue({
                 visible : true,
                 messages : [
                     {
-                        date : '20/03/2020 1 6:30:00',
+                        date : '20/03/2020 16:30:00',
                         text : 'Ciao come stai?',
                         status : 'sent'
                     },
                     {
-                        date : '20/03/2020 1 6:30:55',
+                        date : '20/03/2020 16:30:55',
                         text : 'Bene grazie! Stasera ci vediamo?',
                         status : 'received'
                     },
                     {
-                        date : '20/03/2020 1 6:35:00',
+                        date : '20/03/2020 16:35:00',
                         text : 'Mi piacerebbe ma devo andare a fare la spesa.',
                         status : 'sent'
                     }
@@ -55,17 +55,17 @@ var app = new Vue({
                 visible : true,
                 messages : [
                     {
-                        date : '28/03/2020 1 0:10:40',
+                        date : '28/03/2020 10:10:40',
                         text : 'La Marianna va in campagna',
                         status : 'received'
                     },
                     {
-                        date : '28/03/2020 1 0:20:10',
+                        date : '28/03/2020 10:20:10',
                         text : 'Sicuro di non aver sbagliato chat?',
                         status : 'sent'
                     },
                     {
-                        date : '28/03/2020 1 6:15:22',
+                        date : '28/03/2020 16:15:22',
                         text : 'Ah scusa!',
                         status : 'received'
                     }
@@ -77,12 +77,12 @@ var app = new Vue({
                 visible : true,
                 messages : [
                     {
-                        date : '10/01/2020 1 5:30:55',
+                        date : '10/01/2020 15:30:55',
                         text : 'Lo sai che ha aperto una nuova pizzeria?',
                         status : 'sent'
                     },
                     {
-                        date : '10/01/2020 1 5:50:00',
+                        date : '10/01/2020 15:50:00',
                         text : 'Si, ma preferirei andare al cinema',
                         status : 'received'
                     }
