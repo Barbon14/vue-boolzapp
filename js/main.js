29919f4f577359816cc6dc52eb2f03811c791857
BarbonFisso <barbieri.mat@hotmail.it>
2021-11-16 11:47:50 +0100

    added dinamic activeChat

--- js/main.js
+++ js/main.js
@@ -2,90 +2,97 @@
 var app = new Vue({
     el: "#app",
     data: { 
-        contacts: [
+
+        activeChat : 0,
+        contacts : [
             {
-                name: 'Michele',
-                avatar: 'img/avatar_1.jpg',
-                visible: true,
-                messages: [
+                name : 'Michele',
+                avatar : 'img/avatar_1.jpg',
+                visible : true,
+                messages : [
                     {
-                        date: '10/01/2020 1 5:30:55',
-                        text: 'Hai portato a spasso il cane?',
-                        status: 'sent'
+                        date : '10/01/2020 1 5:30:55',
+                        text : 'Hai portato a spasso il cane?',
+                        status : 'sent'
                     },
                     {
-                        date: '10/01/2020 1 5:50:00',
-                        text: 'Ricordati di dargli da mangiare',
-                        status: 'sent'
+                        date : '10/01/2020 1 5:50:00',
+                        text : 'Ricordati di dargli da mangiare',
+                        status : 'sent'
                     },
                     {
-                        date: '10/01/2020 1 6:15:22',
-                        text: 'Tutto fatto!',
-                        status: 'received'
+                        date : '10/01/2020 1 6:15:22',
+                        text : 'Tutto fatto!',
+                        status : 'received'
                     }
                 ],
             },
             {
-                name: 'Fabio',
-                avatar: 'img/avatar_2.jpg',
-                visible: true,
-                messages: [
+                name : 'Fabio',
+                avatar : 'img/avatar_2.jpg',
+                visible : true,
+                messages : [
                     {
-                        date: '20/03/2020 1 6:30:00',
-                        text: 'Ciao come stai?',
-                        status: 'sent'
+                        date : '20/03/2020 1 6:30:00',
+                        text : 'Ciao come stai?',
+                        status : 'sent'
                     },
                     {
-                        date: '20/03/2020 1 6:30:55',
-                        text: 'Bene grazie! Stasera ci vediamo?',
-                        status: 'received'
+                        date : '20/03/2020 1 6:30:55',
+                        text : 'Bene grazie! Stasera ci vediamo?',
+                        status : 'received'
                     },
                     {
-                        date: '20/03/2020 1 6:35:00',
-                        text: 'Mi piacerebbe ma devo andare a fare la spesa.',
-                        status: 'sent'
+                        date : '20/03/2020 1 6:35:00',
+                        text : 'Mi piacerebbe ma devo andare a fare la spesa.',
+                        status : 'sent'
                     }
                 ],
             },
             {
-                name: 'Samuele',
-                avatar: 'img/avatar_3.jpg',
-                visible: true,
-                messages: [
+                name : 'Samuele',
+                avatar : 'img/avatar_3.jpg',
+                visible : true,
+                messages : [
                     {
-                        date: '28/03/2020 1 0:10:40',
-                        text: 'La Marianna va in campagna',
-                        status: 'received'
+                        date : '28/03/2020 1 0:10:40',
+                        text : 'La Marianna va in campagna',
+                        status : 'received'
                     },
                     {
-                        date: '28/03/2020 1 0:20:10',
-                        text: 'Sicuro di non aver sbagliato chat?',
-                        status: 'sent'
+                        date : '28/03/2020 1 0:20:10',
+                        text : 'Sicuro di non aver sbagliato chat?',
+                        status : 'sent'
                     },
                     {
-                        date: '28/03/2020 1 6:15:22',
-                        text: 'Ah scusa!',
-                        status: 'received'
+                        date : '28/03/2020 1 6:15:22',
+                        text : 'Ah scusa!',
+                        status : 'received'
                     }
                 ],
             },
             {
-                name: 'Luisa',
-                avatar: 'img/avatar_4.jpg',
-                visible: true,
-                messages: [
+                name : 'Luisa',
+                avatar : 'img/avatar_4.jpg',
+                visible : true,
+                messages : [
                     {
-                        date: '10/01/2020 1 5:30:55',
-                        text: 'Lo sai che ha aperto una nuova pizzeria?',
-                        status: 'sent'
+                        date : '10/01/2020 1 5:30:55',
+                        text : 'Lo sai che ha aperto una nuova pizzeria?',
+                        status : 'sent'
                     },
                     {
-                        date: '10/01/2020 1 5:50:00',
-                        text: 'Si, ma preferirei andare al cinema',
-                        status: 'received'
+                        date : '10/01/2020 1 5:50:00',
+                        text : 'Si, ma preferirei andare al cinema',
+                        status : 'received'
                     }
                 ],
             },
         ],
-    }
+    },
+    methods : {
+        setActiveChat(index) {
+            this.activeChat = index;
+        }
+    },
 });
